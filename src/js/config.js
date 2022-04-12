d3f21a059ad1d68d72746b4c7e1839b13a5c1e94
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-04-13 00:17:37 +0300

    try

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RESULT_PER_PAGE = 10;
 export const KEY = '4b96c40c-52f6-4159-a59c-7da486206feb';
 export const MODEL_CLOSE_SE = 2.5;
+const Notthig = '';
